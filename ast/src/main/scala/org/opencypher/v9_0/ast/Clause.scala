f037416847d89f0c3fa4b9f0954ae9fffc0e81f5
Benjamin Kyrlach <ben@thatdot.com>
2024-07-19 13:55:13 -0400

    Code review fixes.

--- ast/src/main/scala/org/opencypher/v9_0/ast/Clause.scala
+++ ast/src/main/scala/org/opencypher/v9_0/ast/Clause.scala
@@ -1538,57 +1538,6 @@ case class SubqueryCall(part: QueryPart, initializations: List[Initialization], 
       checkNoCallInTransactionsInsideRegularCall
   }
 
-//  def checkSubquery: SemanticCheck = {
-//    for {
-////      testVar <- SemanticCheck.fromState { s =>
-////        this.test.folder.treeFold(SemanticCheck.setState(s)) {
-////          case v: Variable =>
-////            acc => {
-////              s.currentScope.localSymbol(v.name) match {
-////                case Some(_) => SkipChildren(acc)
-////                case None =>
-////                  s.declareVariable(v, CTAny.covariant) match {
-////                    case Right(s1) =>
-////                      SkipChildren(for {
-////                        _ <- acc
-////                        r <- SemanticCheck.setState(s1)
-////                      } yield r)
-////                  }
-////              }
-////            }
-////        }
-////      }
-////      _ <- debugCheckState("After declare test", testVar)
-//      outerStateWithImports <- part.checkImportingWith
-//      // Create empty scope under root
-//      _ <- SemanticCheck.setState(outerStateWithImports.state.newBaseScope)
-//      _ <- this.initializations.foldSemanticCheck { i =>
-//        SemanticCheck.fromState { s =>
-//          s.declareVariable(i.variable, CTAny.covariant) match {
-//            case Right(s1) => SemanticCheck.setState(s1)
-//          }
-//        }
-//      }
-//      // Check inner query. Allow it to import from outer scope
-//      innerChecked <- part.semanticCheckInSubqueryContext(outerStateWithImports.state)
-//      _ <- SemanticExpressionCheck.check(Expression.SemanticContext.Results, this.test)
-//      _ <- returnToOuterScope(outerStateWithImports.state.currentScope)
-//      // Declare variables that are in output from subquery
-//      merged <- declareOutputVariablesInOuterScope(innerChecked.state.currentScope.scope)
-//    } yield {
-//      println(s"After inner: ${innerChecked.state.currentScope.scope.symbolTable}")
-//      val importingWithErrors = outerStateWithImports.errors
-//
-//      // Avoid double errors if inner has errors
-//      val allErrors = importingWithErrors ++
-//        (if (innerChecked.errors.nonEmpty) innerChecked.errors else merged.errors)
-//
-//      println(s"Errors: $allErrors")
-//
-//      // Keep errors from inner check and from variable declarations
-//      SemanticCheckResult(merged.state, allErrors)
-//    }
-//  }
   def checkSubquery: SemanticCheck = {
     for {
       outerStateWithImports <- part.checkImportingWith
@@ -1643,12 +1592,7 @@ case class SubqueryCall(part: QueryPart, initializations: List[Initialization], 
 
   private def declareOutputVariablesInOuterScope(rootScope: Scope): SemanticCheck = {
     when(part.isReturning) {
-      val scopeForDeclaringVariables =
-        try {
-          part.finalScope(rootScope)
-        } catch {
-          case _: NoSuchElementException => rootScope
-        }
+      val scopeForDeclaringVariables = part.finalScope(rootScope)
       declareVariables(scopeForDeclaringVariables.symbolTable.values)
     }
   }
